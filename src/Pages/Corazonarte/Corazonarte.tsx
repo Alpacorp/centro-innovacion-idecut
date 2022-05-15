19ff9c128db07ce6d50b5873a857279ca061713a
Alpacorp <alejandro.palacios88@gmail.com>
2022-05-15 06:57:24 -0500

    create initial project

---
+++ src/Pages/Corazonarte/Corazonarte.tsx
@@ -0,0 +1,11 @@
+import "./corazonarte.scss";
+
+const Corazonarte = () => {
+  return (
+    <>
+      <div>Corazonarte</div>
+    </>
+  );
+};
+
+export default Corazonarte;
---
+++ src/Pages/Directory/Directory.tsx
@@ -0,0 +1,11 @@
+import "./directory.scss";
+
+const Directory = () => {
+  return (
+    <>
+      <div>Directory</div>
+    </>
+  );
+};
+
+export default Directory;
