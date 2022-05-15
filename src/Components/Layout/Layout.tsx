a072d717035f089098dd6e3e35204b251e154656
Alpacorp <alejandro.palacios88@gmail.com>
2022-05-15 12:39:11 -0500

    create full initial version page web

---
+++ src/Components/Layout/Layout.tsx
@@ -0,0 +1,132 @@
+import { useEffect, useState } from "react";
+import {
+  arte,
+  bibliotecas,
+  crea,
+  eventos,
+  artesanias,
+  lectura,
+  patrimonio,
+  musica,
+  teatro,
+} from "../assets/images";
+import AnimatedLetters from "../AnimatedLetters/AnimatedLetters";
+import "./layout.scss";
+
+const Layout = () => {
+  const [letterClass, setLetterClass] = useState("text-animate");
+
+  const textArray = [
+    "C",
+    "o",
+    "r",
+    "a",
+    "z",
+    "o",
+    "n",
+    "a",
+    "r",
+    "t",
+    "e",
+    "",
+    "2",
+    "0",
+    "2",
+    "2",
+  ];
+  const textCun = ["C", "u", "n", "d", "i", "n", "a", "m", "a", "r", "c", "a"];
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLetterClass("text-animate-hover");
+    }, 3000);
+  }, []);
+
+  return (
+    <>
+      <section className="layout">
+        <div className="container">
+          <main>
+            <div className="main-content">
+              <div className="main-content-title">
+                <AnimatedLetters
+                  strArray={textArray}
+                  letterClass={letterClass}
+                  idx={1}
+                />
+              </div>
+              <div className="main-content-subtitle">
+                <AnimatedLetters
+                  strArray={textCun}
+                  letterClass={letterClass}
+                  idx={1}
+                />
+              </div>
+              <div className="create">
+                <figure>
+                  <img src={crea} alt="corazonarte 2022 Cundinamarca Crea" />
+                </figure>
+              </div>
+              <div className="first-text">
+                <h1>
+                  Estimulos para personas jurídicas, personas naturales y grupos
+                  constituidos
+                </h1>
+              </div>
+              <div className="second-text">
+                <h2>Más de $1.400 millones</h2>
+                <h3>115 estímulos</h3>
+              </div>
+              <div className="logos">
+                <figure>
+                  <img src={bibliotecas} alt="balance" />
+                </figure>
+                <figure>
+                  <img src={lectura} alt="lectura" />
+                </figure>
+                <figure>
+                  <img src={patrimonio} alt="manualidades" />
+                </figure>
+                <figure>
+                  <img src={teatro} alt="teatro" />
+                </figure>
+                <figure>
+                  <img src={eventos} alt="eventos" />
+                </figure>
+                <figure>
+                  <img src={arte} alt="arte" />
+                </figure>
+                <figure>
+                  <img src={artesanias} alt="fabricación" />
+                </figure>
+                <figure>
+                  <img src={musica} alt="música" />
+                </figure>
+              </div>
+              <div className="categories">
+                <h4>Bibliotecas</h4>
+                <h4>Literatura</h4>
+                <h4>Patrimonio Cultural</h4>
+                <h4>Artes Escénicas</h4>
+                <h4>Eventos</h4>
+                <h4>Artes plásticas y visuales</h4>
+                <h4>Artesanías</h4>
+                <h4>Música</h4>
+              </div>
+            </div>
+            <div className="sidebar">
+              <iframe
+                className="survey-monkey-form"
+                src="https://es.surveymonkey.com/r/22Z3FWL"
+                title="Convocatoria"
+                frameBorder="0"
+              ></iframe>
+            </div>
+          </main>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Layout;
---
+++ src/Components/assets/images/index.ts
@@ -0,0 +1,27 @@
+import mainLogo from "./main-logo.png";
+import cunLogo from "./cun-logo.png";
+import idecutLogo from "./idecut-logo.png";
+import crea from "./crea.png";
+import bibliotecas from "./bibliotecas.png";
+import lectura from "./lectura.png";
+import patrimonio from "./patrimonio.png";
+import teatro from "./teatro.png";
+import eventos from "./eventos.png";
+import arte from "./arte.png";
+import artesanias from "./artesanias.png";
+import musica from "./musica.png";
+
+export {
+  mainLogo,
+  cunLogo,
+  idecutLogo,
+  crea,
+  bibliotecas,
+  lectura,
+  patrimonio,
+  teatro,
+  eventos,
+  arte,
+  artesanias,
+  musica,
+};
